a327bda8c8b32cc97eddde6de74346ce80f4579a
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-01-19 19:44:34 +0300

    grim reaper chasing code

--- scenes/reaper.gd
+++ scenes/reaper.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
 
-
+enum MODES{WANDERING,ATTACKING}
 var hp = 1000
+var mode : MODES = MODES.WANDERING
+
 
 func take_damge():
 	hp -= 20
@@ -11,26 +13,35 @@ func take_damge():
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var random_pos = Vector3.ZERO
+var playable = false
 
+func start_the_game():
+	playable = true
 
+	
+	
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if  !playable:
+		return
+	print(mode)	
+	match mode:
+		MODES.WANDERING:
+			look_at(random_pos)
+			velocity = basis.x * 40
+			move_and_slide()
+			$AnimationPlayer.play("reaper_moving")
+			print("looks")
+			print(random_pos)
+			
+		MODES.ATTACKING:
+			pass
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+func _on_timer_timeout():
+	mode = randi_range(0,1) as MODES
+	match mode:
+		MODES.WANDERING:
+			random_pos = Vector3(randf_range(-118.502,-15.082),randf_range(0,20),randf_range(-223.625,-94.116)) 
+		MODES.ATTACKING:
+			pass
--- scripts/hero.gd
+++ scripts/hero.gd
@@ -22,15 +22,19 @@ func _physics_process(delta):
 	# Add the gravity.
 	#if not is_on_floor():
 	velocity = get_gravity() 
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("action"):
+		$AnimationPlayer.play("hero_attack")
+		$Timer.start()
+		return
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_axis("forward","backward")
 	var rot = Input.get_axis("rot_left","rot_right")
+	
+	
+	
 	$Armature_003.rotate_y(-delta * rot* 5)
 
 	
@@ -60,3 +64,19 @@ func _input(event):
 		$SpringArm3D.rotate_y(0.07  * -clamp( (event as InputEventMouseMotion).relative.x,-1,1)) 
 		$SpringArm3D/Camera3D.rotate_x(0.07  * -clamp( (event as InputEventMouseMotion).relative.y,-1,1)) 
 		$SpringArm3D/Camera3D.rotation_degrees.x = clamp($SpringArm3D/Camera3D.rotation_degrees.x,-47,6.9)
+
+
+func _on_area_3d_body_entered(body):
+	print("hello")
+	pass # Replace with function body.
+
+
+func _on_timer_timeout():
+	$Armature_003/Area3D.monitoring = true
+	$Timer2.start()
+	pass # Replace with function body.
+
+
+func _on_timer_2_timeout():
+	$Armature_003/Area3D.monitoring = false
+	pass # Replace with function body.
