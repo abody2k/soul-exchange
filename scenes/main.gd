0469db0153f5f4f1df4ff3b3db90d4453e962b34
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-01-19 12:36:41 +0300

    starting game, playing first animation, handling camera and handling other animations

--- scenes/main.gd
+++ scenes/main.gd
@@ -18,6 +18,8 @@ func _process(delta):
 func _on_button_button_down():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$CanvasLayer/Control.visible= false
+	$AnimationPlayer.play("start")
+	$Camera3D.current = true
 
 
 func _on_button_2_button_down():
@@ -27,4 +29,16 @@ func _on_button_2_button_down():
 
 func _on_resume_button_down():
 	$CanvasLayer/Control2.visible = false
+	pass # Replace with function body.
+
+
+func _on_animation_player_animation_finished(anim_name):
+	
+	match anim_name:
+		"start":
+			pass
+		"second":
+			pass
+		"third":
+			pass
 	pass # Replace with function body.
