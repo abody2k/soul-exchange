50fa37bdce63d88d3551fe8b73a1b587f72268ae
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-01-20 12:19:50 +0300

    hero taking damage from grim reaper attacks

--- scenes/reaper.gd
+++ scenes/reaper.gd
@@ -68,5 +68,5 @@ func _on_animation_player_animation_finished(anim_name):
 
 
 func _on_area_3d_body_entered(body):
-	print("JUST ATTACKED SOMEBODY")
+	enemy.take_damage(30)
 	pass # Replace with function body.
